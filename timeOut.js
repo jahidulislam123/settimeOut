3f5241fe8166c36260ad0692593d8fea84dd0e6b
unknown <islam15-3879@diu.edu.bd>
2022-03-03 23:44:09 +0600

    first commit

---
+++ timeOut.js
@@ -0,0 +1,17 @@
+function doSomething(){
+    console.log("i am coading javascript");
+}
+
+console.log('i am the first ');
+console.log('second : i am the second person');
+// doSomething();
+setTimeout(doSomething,5000);
+setTimeout(function(){
+    console.log("i am using vs code");
+},4000);
+
+setTimeout(()=>{
+    console.log("exploring mon articles");
+},40)
+console.log('third : ami 3 number bacca ');
+console.log('ami ek hali porno korci');
