d24b1ddcccc5449c941017e89f39d1d535f9cf08
unknown <islam15-3879@diu.edu.bd>
2022-03-04 00:04:25 +0600

    added

--- fetch.js
+++ fetch.js
@@ -1,1 +1,14 @@
 console.log('mama kemon aco');
+console.log(1111);
+console.log(2222);
+// setTimeout(()=>console.log('aaaaa'));
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json));
+console.log(3333);
+console.log(44444);
+
+for(let i=0;i<100;i++){
+    // const element =1000;
+    console.log(i);
+}
