bda04e005dc2cb9a2c522955aca13b336fa184c9
unknown <islam15-3879@diu.edu.bd>
2022-03-04 11:06:44 +0600

    added

---
+++ setinterVal.js
@@ -0,0 +1,15 @@
+setInterval(()=>{
+    // console.log('third');
+},3000);
+
+let seconds=0;
+
+const timeId= setInterval(() => {
+    seconds++;
+    console.log(seconds++);
+
+    if(seconds>15){
+        clearInterval(timeId);
+    }
+}, 1000);
+console.log('seconds');
